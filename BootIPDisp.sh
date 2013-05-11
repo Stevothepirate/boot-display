f12cba2803bfbd5eb8e37e8da92f8afeecbfa0fa
Stevothepirate <steve.dennis@cubic.com>
2013-05-11 18:36:30 +0100

    Removed IP.sh & removed absolute paths in BootIPDisp.sh

--- BootIPDisp.sh
+++ BootIPDisp.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-ip addr list eth0 |grep "inet " |cut -d' ' -f6|cut -d/ -f1 > /home/pi/i2c-disp/IPe.txt
+ip addr list eth0 |grep "inet " |cut -d' ' -f6|cut -d/ -f1 > IPe.txt
 
-ip addr list wlan0 |grep "inet " |cut -d' ' -f6|cut -d/ -f1 > /home/pi/i2c-disp/IPw.txt
+ip addr list wlan0 |grep "inet " |cut -d' ' -f6|cut -d/ -f1 > IPw.txt
 
-sudo python /home/pi/i2c-disp/IP.py &
+sudo python IP.py &
--- IP.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-ip addr list eth0 |grep "inet " |cut -d' ' -f6|cut -d/ -f1 > IPe.txt
-ip addr list wlan0 |grep "inet " |cut -d' ' -f6|cut -d/ -f1 > IPw.txt
